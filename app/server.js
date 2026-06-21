201014d3f84c6ffcc7ab1e90b235e40846a7051f
iamohsin1273 <iamohsin0033@gmail.com>
2026-06-21 14:46:37 +0530

    Add CI/CD project files

---
+++ app/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('DevOps CI/CD Pipeline Running'));
+app.get('/health', (req, res) => res.status(200).send('OK'));
+
+app.listen(3000, () => console.log('Server running on port 3000'));
---
+++ scripts/rollback.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+kubectl rollout undo deployment/devops-demo -n devops-demo
